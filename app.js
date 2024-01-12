dfe19eff532977b21f3d3f48cd89e601705978aa
lczlhw <1179552130@qq.com>
2024-01-13 00:25:50 +0800

    new

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var photoRouter = require('./routes/web/photo');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());/*
-app.use(express.static(path.join(__dirname, 'public')));*/
+app.use(cookieParser());
+app.use(express.static(path.join('/public')));
 
 //app.use('/api', apiPhotoRouter);
 app.use('/', photoRouter);
